319bbd0ae7239bcf9a549e437a20459ae1c7720c
Edriene Paingan <painganedrienejames@gmail.com>
2026-03-03 10:36:04 +0800

    Restructured Client-Server architecture

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, ".env") });
+const express = require("express");
+
+const connectDB = require("./model/db");
+
+const app = express();
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+// View engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "view"));
+
+connectDB().then(() => {
+  const PORT = process.env.PORT || 3000;
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+}).catch((err) => {
+  console.err("Failed to start server:", err.message);
+  process.exit(1);
+});
