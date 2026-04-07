23c86c9c53dfa5fa7ceb6284d758846debfee565
ei-ouf <franz_magbitang@dlsu.edu.ph>
2026-04-07 17:56:07 +0800

    testing a fix for lab tech login on deployment

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   /* config options here */
   reactCompiler: true,
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  }
 };
 
 export default nextConfig;
